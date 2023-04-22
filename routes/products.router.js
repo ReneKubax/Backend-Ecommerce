fd4a31a4d276e5a5aefceb1d5bd22840264b1bfc
ReneKubax <therene21@gmail.com>
2023-04-22 00:51:34 -0600

    integracion de Middlewares

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const productsService = require('./../services/product.service')
 
+
 const router = express.Router();
-const service = new productsService()
+const service = new productsService();
 
 router.get('/', async (req, res) => {
   const products = await service.find();
@@ -13,35 +14,37 @@ router.get('/filter', (req, res) => {
   res.send('Yo soy un filter');
 });
 
-router.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  const product = await service.findOne(id);
-  res.json(product);
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const product = await service.findOne(id);
+    res.json(product);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post('/', async (req, res) => {
   const body = req.body;
-  const newProduct = await service.create(body)
+  const newProduct = await service.create(body);
   res.status(201).json(newProduct);
 });
 
-router.patch('/:id', async (req, res) => {
+router.patch('/:id', async (req, res, next) => {
   try {
     const { id } = req.params;
-  const body = req.body;
-  const product = await service.update(id, body)
-  res.json(product);
+    const body = req.body;
+    const product = await service.update(id, body);
+    res.json(product);
   } catch (error) {
-    res.status(404).json({
-      message: error.message
-    })
+    next(error);
   }
 
 });
 
 router.delete('/:id', async (req, res) => {
   const { id } = req.params;
-  const rta = await service.update(id)
+  const rta = await service.delete(id);
   res.json(rta);
 });
 
